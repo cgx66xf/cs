a58071aaef38d815659b6dac90eda53dbf239311
Taner Paker <taner.2001@hotmail.com>
2022-05-18 16:49:23 +0300

    Update spectrum.js

--- musicvisualiser/musicVis template/spectrum.js
+++ musicvisualiser/musicVis template/spectrum.js
@@ -8,9 +8,9 @@ function Spectrum(){
 		
 		fill(0,255,0)
 		for (var i = 0; i< spectrum.length; i++){
-			var x = map(i, 0, spectrum.length, 0, width);
-		    var h = -height + map(spectrum[i], 0, 255, height, 0);
-		    rect(x, height, width / spectrum.length, h );
+		    var x = map(i, 0, spectrum.length, 0, height);
+		    var h = map(spectrum[i], 0, 255, 0, width);
+		    rect(0, x, h,  height/spectrum.length);
   		}
 	
 		pop();
